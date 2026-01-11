cc2831a304890a5d53075be79de8410830c31bc4
Leeland Barry <110380822+scarvey01@users.noreply.github.com>
2026-01-11 15:43:33 -0500

    FIRST WORKING PORTOTYPE
    he gets stuck a lot because of behavior, not being able to low enough because the ultrasonic sensor is mounted too high, and also the motor drivers sits about 1/8 inch of the ground. But it still works
    
    CURRENT BEHAVIOR:
    - Implemented rolling max distance buffer using last 7 ultrasonic readings to smooth sensor noise.
    - Robot moves forward until max distance <= DISTANCE_THRESHOLD (12 cm), then stops.
    - Added obstacle handling:
      - Beeps buzzer when obstacle detected.
      - Backs up slightly (400 ms) to create pivot space.
      - Scans left (180°) and right (0°) using servo to determine side with more space.
      - Pivots in place toward chosen direction until path is clear or timeout occurs.
    - Servo resets to straight ahead after pivot.

--- Roam-bot.ino
+++ Roam-bot.ino
@@ -1,34 +1,104 @@
-#include "project.h"
-
 #include <Servo.h>
 
-// echo sensor
-#define ECHO_ECHO 4  // they grey wire on this pin has issues...
-#define ECHO_TRIG 7
-#define ECHO_VCC 5  // set this to 5V
-// constants
-#define ECHO_TIMEOUT 100000L    // how long to wait for response
-#define ECHO_TRIG_US 1L         // duration of trigger signal
-#define ECHO_CM_PER_US 0.01723  // centimeters per microsecond (as returned by sensor)
-
-// swivel servo
-#define SERVO_PIN 3
-
-// motor driver
+/* PIN DEFINITIONS */
+// Motors (L298N)
 #define MOTOR_IN1 11
 #define MOTOR_IN2 10
 #define MOTOR_IN3 9
 #define MOTOR_IN4 6
-
-// helpful?
 #define MOTOR_LEFT MOTOR_IN1, MOTOR_IN2
 #define MOTOR_RIGHT MOTOR_IN3, MOTOR_IN4
 
-Servo swivel;
+// Ultrasonic sensor
+#define ECHO_ECHO 4
+#define ECHO_TRIG 7
+#define ECHO_VCC 5
 
-// Read the echo sensor and return the distance reading in microseconds. Multiply by
-// `ECHO_CM_PER_US` to convert from microseconds to centimeters.
+// Servo
+#define SERVO_PIN 3
+
+// Buzzer
+#define BUZZER_PIN 8
+
+/* CONSTANTS */
+#define DISTANCE_THRESHOLD 12   // cm
+#define SAMPLE_COUNT 7          // number of distance samples
+#define ECHO_TIMEOUT 30000      // us
+#define ECHO_TRIG_US 10         // us
+#define ECHO_CM_PER_US 0.01723  // conversion factor
+
+/* GLOBALS */
+Servo swivel;
+long distanceSamples[SAMPLE_COUNT];
+int sampleIndex = 0;
+
+/*  MOTOR STATE ENUM */
+typedef enum MOTOR_STATE {
+  MOTOR_OFF = 0b00,
+  MOTOR_BACKWARD = 0b01,
+  MOTOR_FORWARD = 0b10,
+  MOTOR_OFF2 = 0b11
+} MOTOR_STATE;
+
+/* SETUP  */
+void setup() {
+  // Motors
+  pinMode(MOTOR_IN1, OUTPUT);
+  pinMode(MOTOR_IN2, OUTPUT);
+  pinMode(MOTOR_IN3, OUTPUT);
+  pinMode(MOTOR_IN4, OUTPUT);
+
+  // Ultrasonic sensor
+  pinMode(ECHO_ECHO, INPUT);
+  pinMode(ECHO_TRIG, OUTPUT);
+  pinMode(ECHO_VCC, OUTPUT);
+  digitalWrite(ECHO_VCC, HIGH);
+
+  // Servo
+  swivel.attach(SERVO_PIN);
+  swivel.write(98); // straight ahead
+
+  // Buzzer
+  pinMode(BUZZER_PIN, OUTPUT);
+  digitalWrite(BUZZER_PIN, LOW);
+
+  // Serial
+  Serial.begin(9600);
+
+  // Clear distance samples
+  clearSamples();
+  stopMotors();
+
+  Serial.println("Roam-Bot v1 started");
+}
+
+/* MAIN LOOP */
+void loop() {
+  float distance = echo_get_dist() * ECHO_CM_PER_US;
+  storeSample(distance);
+
+  float maxDist = getMaxSample();
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.print(" cm | Max(");
+  Serial.print(SAMPLE_COUNT);
+  Serial.print("): ");
+  Serial.println(maxDist);
+
+  if (maxDist > DISTANCE_THRESHOLD) {
+    moveForward();
+  } else {
+    beep(2000, 150); // obstacle alert
+    handleObstacle();
+  }
+
+  delay(50);
+}
+
+/*ULTRASONIC SENSOR */
 unsigned long echo_get_dist() {
+  digitalWrite(ECHO_TRIG, LOW);
+  delayMicroseconds(2);
   digitalWrite(ECHO_TRIG, HIGH);
   delayMicroseconds(ECHO_TRIG_US);
   digitalWrite(ECHO_TRIG, LOW);
@@ -36,17 +106,32 @@ unsigned long echo_get_dist() {
   return pulseIn(ECHO_ECHO, HIGH, ECHO_TIMEOUT);
 }
 
-// State of motor according to L298N motor driver module.
-typedef enum MOTOR_STATE {
-  MOTOR_OFF = 0b00,
-  MOTOR_BACKWARD = 0b01,
-  MOTOR_FORWARD = 0b10,
-  MOTOR_OFF2 = 0b11,  // afaik this does the same thing, but check real dual h-bridge drivers
-} MOTOR_STATE;
-// this motor driver is a lot less intuitive than the one(s?) I've used before
+/* DISTANCE BUFFER */
+void storeSample(float value) {
+  distanceSamples[sampleIndex] = value;
+  sampleIndex = (sampleIndex + 1) % SAMPLE_COUNT;
+}
 
-// Set a motor specified by pins in1 and in2 to a given `MOTOR_STATE`. `MOTOR_LEFT` and `MOTOR_RIGHT` can be
-// used in place of pins as convenience: `motor_set_state(MOTOR_LEFT, state)`.
+float getMaxSample() {
+  float maxVal = distanceSamples[0];
+  for (int i = 1; i < SAMPLE_COUNT; i++) {
+    if (distanceSamples[i] > maxVal) maxVal = distanceSamples[i];
+  }
+  return maxVal;
+}
+
+void clearSamples() {
+  for (int i = 0; i < SAMPLE_COUNT; i++) distanceSamples[i] = 300;
+}
+
+/* BUZZER */
+void beep(int frequency, int duration) {
+  tone(BUZZER_PIN, frequency);
+  delay(duration);
+  noTone(BUZZER_PIN);
+}
+
+/* MOTOR CONTROL  */
 void motor_set_state(uint8_t in1, uint8_t in2, char state) {
   bool in1_on = state & 0b10;
   bool in2_on = state & 0b01;
@@ -54,183 +139,81 @@ void motor_set_state(uint8_t in1, uint8_t in2, char state) {
   digitalWrite(in2, in2_on ? HIGH : LOW);
 }
 
-void test_servo() {
-  Serial.println("\nServo sweep from 0 to 180");
-  delay(500);
-  for (int angle = 0; angle <= 180; angle++) {
-    swivel.write(angle);
-    Serial.print("angle: ");
-    Serial.println(angle);
-    delay(10);
-  }
-}
-
-void test_echo() {
-  Serial.println("\nEcho test. Does it look good enough?");
-  delay(500);
-  for (int i = 0; i < 75; i++) {
-    Serial.print("dist: ");
-    Serial.println(echo_get_dist() * ECHO_CM_PER_US);
-    delay(100);
-  }
-}
-
-void test_swivel_echo() {
-  Serial.println("\nServo sweep from 0 to 180, echo along");
-  delay(500);
-
-  float dist_acc = 0;
-
-  for (int angle = 0; angle <= 180; angle += 2) {
-    swivel.write(angle);
-    delay(20);
-    float dist = echo_get_dist() * ECHO_CM_PER_US;
-    dist_acc += dist;
-
-    Serial.print("angle: ");
-    Serial.print(angle);
-    Serial.print("; dist: ");
-    Serial.println(dist);
-  }
-
-  Serial.print("average distance: ");
-  Serial.println(dist_acc / 60);
-}
-
-void test_motors() {
-  Serial.println("\nMotor test: fwd, back, left, right");
-  delay(500);
-
-  Serial.println("forward");
-  motor_set_state(MOTOR_LEFT, MOTOR_OFF);
-  motor_set_state(MOTOR_RIGHT, MOTOR_OFF);
-  delay(200);
+void moveForward() {
   motor_set_state(MOTOR_LEFT, MOTOR_FORWARD);
   motor_set_state(MOTOR_RIGHT, MOTOR_FORWARD);
-  delay(1000);
+}
 
-  Serial.println("backward");
-  motor_set_state(MOTOR_LEFT, MOTOR_OFF);
-  motor_set_state(MOTOR_RIGHT, MOTOR_OFF);
-  delay(200);
+void moveBackward() {
   motor_set_state(MOTOR_LEFT, MOTOR_BACKWARD);
   motor_set_state(MOTOR_RIGHT, MOTOR_BACKWARD);
-  delay(1000);
+}
 
-  Serial.println("left");
-  motor_set_state(MOTOR_LEFT, MOTOR_OFF);
-  motor_set_state(MOTOR_RIGHT, MOTOR_OFF);
-  delay(200);
+void pivotLeft() {
   motor_set_state(MOTOR_LEFT, MOTOR_BACKWARD);
   motor_set_state(MOTOR_RIGHT, MOTOR_FORWARD);
-  delay(1000);
+}
 
-  Serial.println("right");
-  motor_set_state(MOTOR_LEFT, MOTOR_OFF);
-  motor_set_state(MOTOR_RIGHT, MOTOR_OFF);
-  delay(200);
+void pivotRight() {
   motor_set_state(MOTOR_LEFT, MOTOR_FORWARD);
   motor_set_state(MOTOR_RIGHT, MOTOR_BACKWARD);
-  delay(1000);
+}
 
-  Serial.println("done");
+void stopMotors() {
   motor_set_state(MOTOR_LEFT, MOTOR_OFF);
   motor_set_state(MOTOR_RIGHT, MOTOR_OFF);
 }
 
-void test() {
-  test_servo();
-  delay(1000);
+/* OBSTACLE HANDLING */
+void handleObstacle() {
+  Serial.println("Obstacle detected!");
 
-  test_echo();
-  delay(1000);
+  stopMotors();
+  delay(100);
 
-  test_swivel_echo();
-  delay(1000);
+  // back up slightly
+  Serial.println("Backing up...");
+  moveBackward();
+  delay(400);
+  stopMotors();
+  delay(100);
 
-  test_motors();
-  delay(1000);
-}
+  // scan left/right
+  int turnDir = scanForBestDirection();
 
-void test_desired_print_help() {
-  Serial.println(
-    "Enter in a character or sequence of characters to test the corresponding function\r\n"
-    "\t?: Show help\r\n"
-    "\tq: Finish testing\r\n"
-    "\ts: Servo swivel\r\n"
-    "\te: Echo\r\n"
-    "\tl: Swivel + Echo\r\n"
-    "\tm: motor\r\n"
-    "\tr: reset\r\n"
-    "\tn: set servo to degree\r\n"
-    "");
-}
-
-void test_desired() {
-  test_desired_print_help();
-
-  while (true) {
-    switch (Serial.read()) {
-      case '?':
-        test_desired_print_help();
-        continue;
-      case 'q':
-        Serial.println("Quit.");
-        return;
-      case 'r':
-        swivel.write(90);
-        continue;
-
-      // no input
-      case -1:
-        delay(10);
-        continue;
-
-      case 's':
-        test_servo();
-        break;
-      case 'e':
-        test_echo();
-        break;
-      case 'l':
-        test_swivel_echo();
-        break;
-      case 'm':
-        test_motors();
-        break;
-      case 'n':
-        swivel.write(Serial.parseInt());
-        break;
-
-      default:
-        continue;
-    }
-
-    Serial.println("Test complete.");
+  if (turnDir < 0) {
+    Serial.println("Pivoting LEFT");
+    pivotLeft();
+  } else {
+    Serial.println("Pivoting RIGHT");
+    pivotRight();
   }
+
+  // turn until path is clear or timeout
+  unsigned long startTime = millis();
+  while (millis() - startTime < 1500) {
+    float d = echo_get_dist() * ECHO_CM_PER_US;
+    Serial.print("Turning distance: ");
+    Serial.println(d);
+    if (d > DISTANCE_THRESHOLD + 5) break;
+  }
+
+  stopMotors();
+  swivel.write(98); // reset servo
+  clearSamples();
 }
 
-void setup() {
-  pinMode(ECHO_ECHO, INPUT);
-  pinMode(ECHO_TRIG, OUTPUT);
-  pinMode(ECHO_VCC, OUTPUT);
+/* SERVO SCAN */
+int scanForBestDirection() {
+  swivel.write(180); // left
+  delay(300);
+  float leftDist = echo_get_dist() * ECHO_CM_PER_US;
+  Serial.print("Left scan: "); Serial.println(leftDist);
 
-  digitalWrite(ECHO_VCC, HIGH);  // sigh
+  swivel.write(0);   // right
+  delay(300);
+  float rightDist = echo_get_dist() * ECHO_CM_PER_US;
+  Serial.print("Right scan: "); Serial.println(rightDist);
 
-  pinMode(SERVO_PIN, OUTPUT);
-  swivel.attach(SERVO_PIN);
-
-  pinMode(MOTOR_IN1, OUTPUT);
-  pinMode(MOTOR_IN2, OUTPUT);
-  pinMode(MOTOR_IN3, OUTPUT);
-  pinMode(MOTOR_IN4, OUTPUT);
-
-  Serial.begin(BAUD);
-  swivel.write(90);
-
-  //test_desired();
-}
-
-void loop() {
-  test_motors();
+  return (leftDist > rightDist) ? -1 : 1;
 }
